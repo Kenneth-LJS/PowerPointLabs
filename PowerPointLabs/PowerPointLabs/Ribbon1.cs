24a95d37339e0af2d333e24c96ee4ae5815b9a1e
Ken Loh <Kenneth_LJS@Live.com>
2017-01-31 01:08:37 +0800

    Debug button

--- PowerPointLabs/PowerPointLabs/Ribbon1.cs
+++ PowerPointLabs/PowerPointLabs/Ribbon1.cs
@@ -471,7 +471,7 @@ namespace PowerPointLabs
         }
         public string GetAddAnimationButtonLabel(Office.IRibbonControl control)
         {
-            return TextCollection.AddAnimationButtonLabel;
+            return "TEST";
         }
         public string GetInSlideAnimateButtonLabel(Office.IRibbonControl control)
         {
@@ -744,18 +744,51 @@ namespace PowerPointLabs
         //Button Click Callbacks        
         public void AddAnimationButtonClick(Office.IRibbonControl control)
         {
-            try
-            {
-                Globals.ThisAddIn.Application.StartNewUndoEntry();
-
-                AutoAnimate.AddAutoAnimation();
-            }
-            catch (Exception e)
-            {
-                Logger.LogException(e, "AddAnimationButtonClick");
-                throw;
-            }
+            var shape = PowerPointCurrentPresentationInfo.CurrentSelection.ShapeRange[1];
+            string input = shape.Tags["sound type"];
+            ShowInputDialog(ref input);
+            shape.Tags.Add("sound type", input);
         }
+
+        private static DialogResult ShowInputDialog(ref string input)
+        {
+            System.Drawing.Size size = new System.Drawing.Size(200, 70);
+            Form inputBox = new Form();
+
+            inputBox.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            inputBox.ClientSize = size;
+            inputBox.Text = "Name";
+
+            System.Windows.Forms.TextBox textBox = new TextBox();
+            textBox.Size = new System.Drawing.Size(size.Width - 10, 23);
+            textBox.Location = new System.Drawing.Point(5, 5);
+            textBox.Text = input;
+            inputBox.Controls.Add(textBox);
+
+            Button okButton = new Button();
+            okButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+            okButton.Name = "okButton";
+            okButton.Size = new System.Drawing.Size(75, 23);
+            okButton.Text = "&OK";
+            okButton.Location = new System.Drawing.Point(size.Width - 80 - 80, 39);
+            inputBox.Controls.Add(okButton);
+
+            Button cancelButton = new Button();
+            cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            cancelButton.Name = "cancelButton";
+            cancelButton.Size = new System.Drawing.Size(75, 23);
+            cancelButton.Text = "&Cancel";
+            cancelButton.Location = new System.Drawing.Point(size.Width - 80, 39);
+            inputBox.Controls.Add(cancelButton);
+
+            inputBox.AcceptButton = okButton;
+            inputBox.CancelButton = cancelButton;
+
+            DialogResult result = inputBox.ShowDialog();
+            input = textBox.Text;
+            return result;
+        }
+
         public void ZoomBtnClick(Office.IRibbonControl control)
         {
             Globals.ThisAddIn.Application.StartNewUndoEntry();
